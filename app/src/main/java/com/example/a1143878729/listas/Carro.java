5ae13585e191d615f7a50513c44d75e375bf7ff8
Javier <jtrochez.jate@gmail.com>
2018-08-28 12:58:26 -0500

    se probo el codigo

---
+++ app/src/main/java/com/example/a1143878729/listas/Carro.java
@@ -0,0 +1,17 @@
+package com.example.a1143878729.listas;
+
+public class Carro implements Volador {
+    String marca;
+    String modelo;
+    String kilometraje;
+
+    @Override
+    public void volar() {
+
+    }
+
+    @Override
+    public void aterrizar() {
+
+    }
+}
---
+++ app/src/main/java/com/example/a1143878729/listas/Volador.java
@@ -0,0 +1,6 @@
+package com.example.a1143878729.listas;
+
+public interface Volador {
+    public void volar();
+    public void aterrizar();
+}
